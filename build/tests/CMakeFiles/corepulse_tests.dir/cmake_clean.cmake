65b40b5bddde2607aeb6c6baf453e5735134685a
DingoOz <dingo.australia@gmail.com>
2025-06-18 12:47:14 +1000

    foundation code

---
+++ build/tests/CMakeFiles/corepulse_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/corepulse_tests.dir/test_main.cpp.o"
+  "CMakeFiles/corepulse_tests.dir/test_main.cpp.o.d"
+  "corepulse_tests"
+  "corepulse_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/corepulse_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dingo/Programming/CorePulse/tests
+# Build directory: /home/dingo/Programming/CorePulse/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[corepulse_tests]=] "/home/dingo/Programming/CorePulse/build/tests/corepulse_tests")
+set_tests_properties([=[corepulse_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/dingo/Programming/CorePulse/tests/CMakeLists.txt;15;add_test;/home/dingo/Programming/CorePulse/tests/CMakeLists.txt;0;")
